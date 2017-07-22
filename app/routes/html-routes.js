26eeee4660ec8b268fe72d48b23fd947fe1e93e7
btarman <btarman75@gmail.com>
2017-07-22 11:49:47 -0400

    routing files

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -2,31 +2,42 @@ var path = require('path');
 
 module.exports = function (app, passport) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'))
+        res.render("index");
     });
 
-    app.get('/bio', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/bio.html'))
+    app.get('/about', function (req, res) {
+        res.render("about");
     });
     app.get('/goodies', function (req, res) {
-    	res.sendFile(path.join(__dirname, '../public/goodies.html'))
+        res.render("goodies");
     })
     app.get('/contact', function (req, res) {
-    	res.sendFile(path.join(__dirname, '../public/contact.html'))
+        res.render("contact");
     })
     app.get('/recommendations', function (req, res) {
-    	res.sendFile(path.join(__dirname, '../public/recommendations.html'))
+        res.render("recommendations");
     })
     // added loggedIn for the next two routes to correspond with passport
-    app.get('/order', loggedIn, function (req, res, next) {
-    	res.sendFile(path.join(__dirname, '../public/order.html'))
-
+    app.get('/submitGoodie', function (req, res) {
+        if (!req.user) {
+            res.redirect("/login")
+        } else {
+            res.render("submitGoodie");
+        }
     })
-    app.get('/shopping-cart', loggedIn, function (req, res, next) {
-    	res.sendFile(path.join(__dirname, '../public/shopping-cart.html'))
+    app.get('/cart', function (req, res) {
+        if (!req.user) {
+            res.redirect("/login")
+        } else {
+            res.render("cart");
+        }
     })
-    app.get('order-history', loggedIn, function (req, res, next) {
-    	res.sendFile(path.join(__dirname, '../public/order-history.html'))
+    app.get('profile', function (req, res) {
+        if (!req.user) {
+            res.redirect("/login")
+        } else {
+            res.render("profile");
+        }
     })
     // this is a boiler plate function for using passport. Not entirely sure on the passport side of things
     function loggedIn(req, res, next) {
